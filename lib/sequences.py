7bf8b9d724f7df61d57462b958fb818b702b3fe2
Medy <mannomedy@gmail.com>
2025-08-13 09:32:14 +0300

    add changes

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    sequence = []
+
+    if length > 0:
+        sequence.append(0)
+    if length > 1:
+        sequence.append(1)
+
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)
